3f038dc1675428c6847bf103537e09a02576f026
Coby Yates <cobylyates@gmail.com>
2020-03-14 17:34:11 -0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { routes } from "./routes";
 
-Vue.config.productionTip = false
+import store from "./store";
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  router: router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import Home from "./components/Home";
+
+
+export const routes = [
+    { path: '', component: Home },
+]
