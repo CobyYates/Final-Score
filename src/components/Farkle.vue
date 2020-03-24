c7437b1a1a56b5d1774bc2ac12ba15fdee8660b4
Quinn <quinn.nilsson@gmail.com>
2020-03-24 17:51:47 -0600

    changed text area sizing

--- src/components/Farkle.vue
+++ src/components/Farkle.vue
@@ -1,112 +1,103 @@
 <template>
   <v-container>
-    <form>
-      <div id="player1">
-        <v-text-field
-        v-model="name"
-        :error-messages="nameErrors"
-        label="Name"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-      ></v-text-field>
-      
-      <div class="mt-4">
-        <v-textarea 
-        :outlined="outlined"
-        background-color="blue"
-        color="black"
-        label="Blue Score"
-      ></v-textarea>
-      </div>
-      
+    <v-row>
+      <v-col cols="12" sm="6">
+      <form>
+        <div id="player1">
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>
 
-      <v-textarea 
-        background-color="red"
-        color="black"
-        label="Red Score"
-      ></v-textarea>
+          <div class="mt-4">
+            <v-textarea
+              :outlined="outlined"
+              background-color="blue"
+              color="black"
+              label="Blue Score"
+            ></v-textarea>
+          </div>
 
-      <v-textarea 
-        background-color="yellow"
-        color="black"
-        label="Yellow Score"
-      ></v-textarea>
+          <v-textarea background-color="red" color="black" label="Red Score"></v-textarea>
 
-      <v-textarea 
-        background-color="green"
-        color="black"
-        label="Green Score"
-      ></v-textarea>
-      </div>
-      
-  
-      <v-btn class="mr-4" @click="submit">submit</v-btn>
-      <v-btn @click="clear">clear</v-btn>
-    </form>
+          <v-textarea background-color="yellow" color="black" label="Yellow Score"></v-textarea>
+
+          <v-textarea background-color="green" color="black" label="Green Score"></v-textarea>
+        </div>
+
+        <v-btn class="mr-4" @click="submit">submit</v-btn>
+        <v-btn @click="clear">clear</v-btn>
+      </form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      name:null,
-      redScore:0,
-      blueScore:0,
-      greenScore:0,
-      yellowScore:0,
-      penaltyScore:0,
-      totalScore:0
-    }),
+export default {
+  data: () => ({
+    name: null,
+    redScore: 0,
+    blueScore: 0,
+    greenScore: 0,
+    yellowScore: 0,
+    penaltyScore: 0,
+    totalScore: 0
+  }),
 
-    computed: {
-      form () {
-        return {
-          name: this.name,
-          address: this.address,
-          city: this.city,
-          state: this.state,
-          zip: this.zip,
-          country: this.country,
-        }
-      },
+  computed: {
+    form() {
+      return {
+        name: this.name,
+        address: this.address,
+        city: this.city,
+        state: this.state,
+        zip: this.zip,
+        country: this.country
+      };
+    }
+  },
+
+  watch: {
+    name() {
+      this.errorMessages = "";
+    }
+  },
+
+  methods: {
+    addressCheck() {
+      this.errorMessages =
+        this.address && !this.name ? "Hey! I'm required" : "";
+
+      return true;
     },
+    resetForm() {
+      this.errorMessages = [];
+      this.formHasErrors = false;
 
-    watch: {
-      name () {
-        this.errorMessages = ''
-      },
+      Object.keys(this.form).forEach(f => {
+        this.$refs[f].reset();
+      });
     },
+    submit() {
+      this.formHasErrors = false;
 
-    methods: {
-      addressCheck () {
-        this.errorMessages = this.address && !this.name
-          ? 'Hey! I\'m required'
-          : ''
+      Object.keys(this.form).forEach(f => {
+        if (!this.form[f]) this.formHasErrors = true;
 
-        return true
-      },
-      resetForm () {
-        this.errorMessages = []
-        this.formHasErrors = false
-
-        Object.keys(this.form).forEach(f => {
-          this.$refs[f].reset()
-        })
-      },
-      submit () {
-        this.formHasErrors = false
-
-        Object.keys(this.form).forEach(f => {
-          if (!this.form[f]) this.formHasErrors = true
-
-          this.$refs[f].validate(true)
-        })
-      },
-    },
+        this.$refs[f].validate(true);
+      });
+    }
   }
+};
 </script>
 
 <style scoped>
-
+#player1 {
+  width: 50%;
+}
 </style>
