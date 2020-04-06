8961f856bb7c8f3fad632632682ea707492951bb
Quinn <quinn.nilsson@gmail.com>
2020-04-06 17:23:09 -0600

    made buttons, title, input field.

--- src/views/games/Qwixx.vue
+++ src/views/games/Qwixx.vue
@@ -1,162 +1,51 @@
 <template>
-	
+	<v-container>
+		<v-row class="mx-4 my-4">
+			<h1 class="display-3" underlined="true">Qwixx</h1>
+		</v-row>
+		<v-row>
+			<input class="mx-4 my-4" v-model="name" placeholder="Put name here" />
+			<v-btn outlined="true" color="purple">Submit</v-btn>
+		</v-row>
+		<v-row>
+			<v-col>
+				<v-btn class="mx-2" color="red" dark v-for="(item, i) in 11" :key="item.i">{{ i + 2}}</v-btn>
+				<v-btn>Lock</v-btn>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col>
+				<v-btn class="mx-2" color="blue" dark v-for="(item, i) in 11" :key="item.i">{{ i + 2}}</v-btn>
+				<v-btn>Lock</v-btn>
+			</v-col>
+		</v-row>
 
-<v-card flat>
-      <v-card-text>
-        <v-container fluid>
-          <v-row>
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="red"
-                color="red"
-                value="red"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="red darken-3"
-                color="red darken-3"
-                value="red darken-3"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="indigo"
-                color="indigo"
-                value="indigo"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="indigo darken-3"
-                color="indigo darken-3"
-                value="indigo darken-3"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="orange"
-                color="orange"
-                value="orange"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="orange darken-3"
-                color="orange darken-3"
-                value="orange darken-3"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-  
-          <v-row class="mt-12">
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="primary"
-                color="primary"
-                value="primary"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="secondary"
-                color="secondary"
-                value="secondary"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="success"
-                color="success"
-                value="success"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="info"
-                color="info"
-                value="info"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-            <v-col cols="12" sm="4" md="4">
-              <v-checkbox
-                v-model="ex4"
-                label="warning"
-                color="warning"
-                value="warning"
-                hide-details
-              ></v-checkbox>
-              <v-checkbox
-                v-model="ex4"
-                label="error"
-                color="error"
-                value="error"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
+		<v-row>
+			<v-col class="d-flex flex-row-reverse justify-end">
+				<v-btn>Lock</v-btn>
+				<v-btn class="mx-2" color="green" dark v-for="(item, i) in 11" :key="item.i">{{ i + 2}}</v-btn>
+			</v-col>
+		</v-row>
 
-
-	<!-- <div class="mt-4">
-              <v-textarea
-								v-model="blueScore"
-                :outlined="outlined"
-                background-color="blue"
-                color="black"
-                label="Blue Score"
-              ></v-textarea>
-            </div>
-
-           <div class="mt-4">
-              <v-textarea
-								v-model="redScore"
-                :outlined="outlined"
-                background-color="red"
-                color="black"
-                label="Red Score"
-              ></v-textarea>
-            </div>
-
-            <div class="mt-4">
-              <v-textarea
-								v-model="yellowScore"
-                :outlined="outlined"
-                background-color="yellow"
-                color="black"
-                label="Yellow Score"
-              ></v-textarea>
-            </div>
-
-            <div class="mt-4">
-              <v-textarea
-								v-model="greenScore"
-                :outlined="outlined"
-                background-color="green"
-                color="black"
-                label="Green Score"
-              ></v-textarea>
-            </div>
-	</div>-->
+		<v-row>
+			<v-col class="d-flex flex-row-reverse justify-end">
+				<v-btn>Lock</v-btn>
+				<v-btn
+					class="mx-2"
+					color="yellow darken-1"
+					dark
+					v-for="(item, i) in 11"
+					:key="item.i"
+				>{{ i + 2}}</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			checkedNames: [],
 			name: null,
 			redScore: 0,
 			blueScore: 0,
@@ -181,15 +70,4 @@ export default {
 </script>
 
 <style scoped>
-.container {
-	display: flex;
-	border: 1px solid blue;
-}
-.row {
-	padding: 10px;
-	border: 1px solid orange;
-}
-#player1 {
-	width: 100%;
-}
 </style>
