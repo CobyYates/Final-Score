4c2253470c8e8107e4f7398e87beb42db8c25868
Quinn <quinn.nilsson@gmail.com>
2020-04-13 17:20:42 -0600

    formatting got messed up, thats why the conflicts

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@
 				<template v-for="item in items">
 					<v-row v-if="item.heading" :key="item.heading" align="center" :to="item.to">
 						<v-col cols="6">
-							<v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+							<v-subheader v-if="item.heading">
+								{{ item.heading }}
+							</v-subheader>
 						</v-col>
 						<v-col cols="6" class="text-center">
 							<a href="#!" class="body-2 black--text">EDIT</a>
@@ -27,16 +29,22 @@
 						:key="item.text"
 						v-model="item.model"
 						:prepend-icon="item.model ? item.icon : item['icon-alt']"
-						append-icon
+						append-icon=""
 					>
 						<template v-slot:activator>
 							<v-list-item-content>
-								<v-list-item-title>{{ item.text }}</v-list-item-title>
+								<v-list-item-title>
+									{{ item.text }}
+								</v-list-item-title>
 							</v-list-item-content>
 						</template>
-						<v-list-item v-for="(child, i) in item.children" :key="i" link :to="child.to">
+						<v-list-item 
+							v-for="(child, i) in item.children" 
+							:key="i"  
+							:to="child.to"
+							link>
 							<v-list-item-action v-if="child.icon">
-								<v-icon>{{ child.icon }}</v-icon>
+								<v-icon :color="child.color">{{ child.icon }}</v-icon>
 							</v-list-item-action>
 							<v-list-item-content>
 								<v-list-item-title>{{ child.text }}</v-list-item-title>
